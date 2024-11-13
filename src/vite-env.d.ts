cf8581098bc53e580681ffdb97a409e5ca0cb414
RevanSP <revanspstudy28@gmail.com>
2024-11-13 21:35:35 +0700

    Initial commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+declare var CodeMirror: any;
+
+    // "deploy": "npm run build && gh-pages -d dist"
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+  }
+})
+
+
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   base: '/CHATBOT-AI/',  
+// })
